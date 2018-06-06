8d0f9041ca89550e3041050053d6e5ced3846907
魏凯 <weikaiii@sina.cn>
2018-06-06 13:43:44 +0800

    init

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Jobs\SendMailQueue;
+use App\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function store()
+    {
+       $users = User::where('id','>','24')->get();
+       foreach ($users as $user){
+           $this->dispatch(new SendMailQueue($user));
+       }
+       echo "Done";
+    }
+}
